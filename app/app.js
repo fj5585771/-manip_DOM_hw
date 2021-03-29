c106912e77cedbf8044b2d5adb75a4750299daf0
fraser jamieson <fraserjamieson.93@gmail.com>
2021-03-29 19:16:17 +0100

    extensions

--- app/app.js
+++ app/app.js
@@ -1,96 +1,63 @@
 document.addEventListener('DOMContentLoaded', () => { 
 
-    // GRAB WHOLE FORM SECTION FOR DOM MANIP.
-    const wholeForm = document.querySelector('#new-film-form');
-    wholeForm.addEventListener('submit', handleFormSubmission);
+    const gatherForm = document.querySelector('#new-film-form');
+    gatherForm.addEventListener('submit', handleFormSubmission);
 
-    // EVENT LISTENER FOR RESETTING FORM INPUT VALUES
+    const createList = document.querySelector('#new-film-form');
+    createList.addEventListener('submit', createFilmListItem);
+
     const clearingInputs = document.querySelector('#new-film-form');
     clearingInputs.addEventListener('submit', handleClearingInputs);
 
-    // EVENT LISTENER FOR CLEAR LIST ITEMS BUTTON
     const clearList = document.querySelector('#film-list');
-    clearList.addEventListener('click', handleClearingListItems);
-
+    clearList.addEventListener('click', handleClearingListItems)
 });
 
 const handleFormSubmission = function (event) {
-
     event.preventDefault();
-    
-    // CREATE DIV FOR COLLECTIVE INPUTS CHILD WRAPPER
+    const newListItem = createFilmListItem(event.target);
+    const parentList = document.querySelector('#film-list');
+    parentList.appendChild(newListItem);
+    event.target.reset();
+};
 
-    const formDivChild = document.createElement('div');
-
-    // GIVE DIV CHILD CLASS NAME - for later styling
-
-    formDivChild.classList.add('film-box');
-
-
-    // CREATE VARIABLES FOR INPUT FIELD VALUES;
-
-    const title = event.target.title.value;
-    const director = event.target.director.value;
-    const genre = event.target.genre.value;
-
+const createFilmListItem = function (event) {
     // CREATE DIV AND CHILD LI ELEMENTS FOR INSERTION INTO FILM UL;
-
-    const titleEntry = document.createElement('li');
+    const title = event.title.value;
+    const director = event.director.value;
+    const genre = event.genre.value;
+    const titleEntry = document.createElement('h3');
     titleEntry.textContent = title
-    const directorEntry = document.createElement('li');
+    const directorEntry = document.createElement('p');
     directorEntry.textContent = director;
-    const genreEntry = document.createElement('li');
+    const genreEntry = document.createElement('p');
     genreEntry.textContent = genre;
 
+    // THEN APPEND CHILD LIST TO PARENT DIV
+    const newListItem = document.createElement('li');
+    newListItem.appendChild(titleEntry);
+    newListItem.appendChild(directorEntry);
+    newListItem.appendChild(genreEntry);
 
-    // APPEND NEW CHILD DIV ('LI') ELEMENTS TO PARENT DIV (DIV THAT WILL BE APPENDED TO ('#FILM-LIST))
+    // CREATE BUTTON AND APPEND
+    const button = document.createElement('button');
+    button.classList.add('button');
+    button.textContent = 'delete'; 
+    newListItem.appendChild(button); 
 
-    formDivChild.appendChild(titleEntry);
-    formDivChild.appendChild(directorEntry);
-    formDivChild.appendChild(genreEntry);
-
-    // THEN APPEND DIV CHILDREN TO FILM LIST
-    const filmItems = document.querySelector('#film-list');
-    filmItems.appendChild(formDivChild);
-
-    // CREATE BUTTON CHILD
-
-    const buttonDiv = document.createElement('button');
-
-    // ADD CLASS NAME TO BUTTON DIV FOR STYLING
-
-    buttonDiv.classList.add('button1');
-    buttonDiv.textContent = ('Remove selection'); 
-
-    // APPEND BUTTON TO FORM - EDIT: CHANGED TO APPEND ON UL 
-    filmItems.appendChild(buttonDiv);   
-    console.log(event.target)
-
-    // THIS WILL CALL BUTTON EACH TIME THE FORM SUBMISSION IS MADE - WE CAN USE THIS TO CLEAR EACH ENTRY INPUT **
-
-        // ** THE NEWLY CREATED BUTTON MUST HAVE ID TO REFER TO. SO THAT WHEN STORING TARGETED VALUES IN OBJECTS, 
-
-        // E.G (CONST REMOVEBUTTON - EVENT.TARGET.BUTTON.VALUE)
-
-        // WE DONT WANT TO STORE IT INSIDE CREATED ELEMENT AS TEXT CONTENT, WE WANT IT TO REMOVE INDIVIDUAL ENTRY THAT IT IS BEING CALLED ON
-
-};
+    return newListItem;
+}
 
 const handleClearingInputs = function() {
     // RESET FORM INPUT FIELDS
     const resetForm = document.querySelector('#new-film-form');
     resetForm.reset();
-
 };
 
-const handleClearingListItems = function (event) {
+const handleClearingListItems = function () {
     // RESET FILM LIST ITEMS
-    let formDivChild = document.querySelector(".film-box");
-    let listContent = formDivChild.innerHTML;
-    // formDivChild.setAttribute("id", "film-box-id");
-    formDivChild.innerHTML = '';
-    console.log(event.target);
+    const newList = document.querySelector("#film-list");
+    newList.innerHTML ='';
+    
 
-    // const parentDiv = formDivChild.parentnode;
-    // parentDiv.removeChild(formDivChild);
 }
